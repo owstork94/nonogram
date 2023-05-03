30be76e9b5a8d05e266628b915e85823f69874ee
owstork94 <owstork94@gmail.com>
2023-05-03 18:00:07 +0900

    노노그램 행 열 입력

--- src/nonogram.java
+++ src/nonogram.java
@@ -1,3 +1,12 @@
-public class nonogram {
+// import java.util.Scanner;
 
-}
+// public class nonogram{
+// Scanner scan = new Scanner(System.in);
+
+// System.out.println("row : ");
+// int row = scan.nextInt();
+// System.out.println("col : ");
+// int col = scan.nextInt();
+
+// int [][] test = new int[row][col];
+// }
